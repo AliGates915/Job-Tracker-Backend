0f5a0419027de1e4ffb360f82abeb27f75118ad2
Ali Gates <128673394+AliGates915@users.noreply.github.com>
2026-05-11 13:51:28 +0500

    Update application.routes.js

--- modules/application/application.routes.js
+++ modules/application/application.routes.js
@@ -11,7 +11,7 @@ import { authenticate } from "../../middlewares/auth.middleware.js";
 const router = express.Router();
 router.use(authenticate); // protect all routes
 
-router.post("/", createApplication);
+router.post("/", authenticate, createApplication);
 router.get("/", getApplications);
 router.put("/:id", updateApplication);
 router.delete("/:id", deleteApplication);
