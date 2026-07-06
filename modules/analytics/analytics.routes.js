a055d9cee0c50ee7715fb7cb238c24b76e4683d0
Ali Gates <128673394+AliGates915@users.noreply.github.com>
2026-07-06 23:12:41 +0500

    Add authentication middleware to analytics route

--- modules/analytics/analytics.routes.js
+++ modules/analytics/analytics.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAnalytics } from "./analytics.controller.js";
+import { authenticate } from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getAnalytics);
+router.get("/", authenticate, getAnalytics);
 
 export default router;
